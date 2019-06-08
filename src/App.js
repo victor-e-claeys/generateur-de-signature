5d8c7e10fe24f7b2c15858d264e1e4114f88f5c3
victor-e-claeys <ecrire@vincentclaeys.com>
2019-06-08 09:46:52 -0400

    Working copy button, snackbar messages, responsive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import {Button, Paper, Grid, TextField} from '@material-ui/core';
+import {Button, Paper, Grid, Snackbar, TextField} from '@material-ui/core';
 import Signature from './signatures/blancmarine';
 import { saveAs } from 'file-saver';
 import './App.css';
@@ -12,8 +12,24 @@ class App extends React.Component {
       name: 'Votre nom',
       title: 'Votre titre',
       telephone: '1234567890',
-      email: 'exemple@email.com'
+      email: 'exemple@email.com',
+      message: null
     };
+  }
+  copySignature = () => {
+    const node = document.getElementById('signature');
+    const selection = window.getSelection();
+    const range = document.createRange();
+    try{
+      range.selectNodeContents(node);
+      selection.removeAllRanges();
+      selection.addRange(range);
+      document.execCommand('copy');
+      selection.removeAllRanges();
+      this.setState({message: 'La signature a été copiée dans le presse-papier.'});
+    }catch(e){
+      this.setState({message: 'Cette fonctionnalitée est indisponible dans votre navigateur web.'});
+    }
   }
   setValue = key => ({target: {value}}) => {
     this.setState({
@@ -21,39 +37,40 @@ class App extends React.Component {
     });
   }
   render(){
+    const {message} = this.state;
     const {setValue} = this;
-    const signature = <Signature {...this.state} {...this} />;
+    const signature = <div id="signature"><Signature {...this.state} {...this} /></div>;
     const signatureHtml = ReactDOMServer.renderToStaticMarkup(signature);
     return (
       <Grid container>
-        <Grid item xs={6}>
+        <Grid item xs={12} md={6}>
           {signature}
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={12} md={6}>
           <Paper style={{padding:10}}>
             <Grid container spacing={2}>
-              <Grid item xs={6}>
+              <Grid item xs={12} md={6}>
                 <TextField
                   fullWidth
                   label="Nom"
                   onChange={setValue('name')}
                 />
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={12} md={6}>
                 <TextField
                   fullWidth
                   label="Titre"
                   onChange={setValue('title')}
                 />
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={12} md={6}>
                 <TextField
                   fullWidth
                   label="Téléphone (chiffres seulement)"
                   onChange={setValue('telephone')}
                 />
               </Grid>
-              <Grid item xs={6}>
+              <Grid item xs={12} md={6}>
                 <TextField
                   fullWidth
                   label="Email"
@@ -62,10 +79,16 @@ class App extends React.Component {
               </Grid>
               <Grid item xs={12}>
                 <Button variant="contained" color="primary" onClick={() => saveAs(new Blob([signatureHtml], {type: "text/html;charset=utf-8"}), 'signature.html')}>Enregistrer</Button>
+                <Button variant="contained" color="secondary" onClick={this.copySignature}>Copier</Button>
               </Grid>
             </Grid>
           </Paper>
         </Grid>
+        <Snackbar 
+          autoHideDuration={5000} 
+          onClose={() => this.setState({message:null})}
+          open={!!message} 
+          message={message} />
       </Grid>
     );
   }
