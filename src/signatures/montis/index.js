01c3d35af0c4e1c05e3ae13aa971154708a9b921
victor-e-claeys <ecrire@vincentclaeys.com>
2019-07-09 09:41:35 -0400

    Fix Montis signature

--- src/signatures/montis/index.js
+++ src/signatures/montis/index.js
@@ -19,68 +19,38 @@ class Montis extends React.Component {
     const {email, name, title, telephone, mobile} = this.props;
     return(
       <table cellSpacing={0} cellPadding={0}>
-        <tbody><tr>
-            <td height={20} colSpan={3}>
-              <p style={{fontSize: '16px', color: '#000000', fontFamily: '"Raleway", Arial, Helvetica, sans-serif', marginBottom: '30px', marginTop: '0px', fontWeight: 700, letterSpacing: '1px'}}>
-                {name}
+        <tbody>
+          <tr>
+            <td valign="middle" style={{paddingRight: 16}}>
+              <img src={logo} width={130} border={0} />
+            </td>   
+            <td style={{borderLeft:'solid 1px #000', paddingLeft:16}}>
+              <p style={{fontFamily: '"Raleway", Arial, Helvetica, sans-serif', marginTop: '0px', marginBottom:'12px'}}>
+                <span style={{color: '#4f2b82', fontSize: '16px', textTransform: 'uppercase', fontWeight: 700}}>
+                  {name}
+                </span>
                 <br />
-                <span style={{fontSize: '13px', color: '#4f2b82', fontFamily: 'Arial, Helvetica, sans-serif', fontWeight: 'normal', letterSpacing: 0}}>
+                <span style={{fontSize: '14px', color: '#58595b', fontFamily: 'Arial, Helvetica, sans-serif'}}>
                   {title}
                 </span>
               </p>
-            </td>
-          </tr>
-          <tr>
-            <td width={121} rowSpan={3} valign="top">
-              <p>
-                <img src={logo} alt="facebook" name="klambi2" border={0} align="bottom" id="klambi2" />
-              </p>
-            </td>   
-            <td width={27} />
-            <td>
-              <p style={{fontSize: '14px', color: '#58595b', fontFamily: 'Arial, Helvetica, sans-serif', marginBottom: '5px', marginTop: '5px', lineHeight: '1.3'}}>
-                Téléphone:  {telephone}
+              <p style={{fontSize: '14px', color: '#58595b', fontFamily: 'Arial, Helvetica, sans-serif', marginTop: '0px', marginBottom:'6px', lineHeight: '1'}}>
+                Tél. {telephone}
                 {
                   mobile ? 
-                  <span>&nbsp;<span style={{color: '#4f2b82', textDecoration: 'none'}}>—</span>&nbsp;Cellulaire {mobile}</span> : 
+                  <span>&nbsp;&nbsp;<span style={{color: '#4f2b82', textDecoration: 'none'}}>—</span>&nbsp;&nbsp;Cell. {mobile}</span> : 
                   null
                 }
-                <br />
               </p>
-            </td>  
-          </tr>
-          <tr>
-            <td width={27} />
-            <td height={27} valign="middle" bgcolor="#4f2b82">
-              <p style={{fontFamily: 'Arial, Helvetica, sans-serif', color: '#fff', fontSize: '13px'}}>
+              <p style={{fontFamily: 'Arial, Helvetica, sans-serif', fontWeight: 'bold', color: '#4f2b82', fontSize: '14px', marginTop: '0px', marginBottom:'6px'}}>
                 {/* nom de l'association */}
-                &nbsp;&nbsp;ASSOCIATION DE SOCCER MONTIS 
-              </p>
-            </td>
-          </tr>
-          <tr>
-            <td width={27} />
-            <td><p style={{fontFamily: 'Arial, Helvetica, sans-serif', fontSize: '14px', color: '#58595b', marginBottom: '10px', marginTop: '6px'}}>  
-                Courriel:
-                <a href={'mailto:' + email} style={{color: '#4f2b82', textDecoration: 'none'}}>
-                  {email}
-                </a>
-                &nbsp;&nbsp;
-                <span style={{color: '#4f2b82', textDecoration: 'none'}}>
-                  — 
-                </span>
-                &nbsp; 
-                {/* SITE WEB  */}
                 <a href="https://asmontis.com" style={{color: '#4f2b82', textDecoration: 'none'}}>
-                  www.asmontis.com
+                  Nous sommes asmontis.com 
                 </a>
               </p>
-            </td>
-          </tr>
-          <tr>
-            <td width={121} />
-            <td width={27} />
-            <td>
+              <p style={{fontFamily: 'Arial, Helvetica, sans-serif', fontSize: '14px', color: '#58595b', marginBottom: '10px', marginTop: '6px'}}>  
+                1780, rue Roberval, Saint-Bruno-de-Montarville, QC  J3V 3R3 
+              </p>
               <p style={{marginBottom: 0, marginTop: '10px'}}>
                 {/* FACEBOOK */}
                 <a href="https://www.facebook.com/ASMONTIS" target="_blank">
