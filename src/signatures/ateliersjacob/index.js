a7b6400763674ab80c570e49a7579d669c71ec8f
Vincent Claeys <ecrire@victorconceptum.com>
2020-08-26 08:48:29 -0400

    Corrections AJ

--- src/signatures/ateliersjacob/index.js
+++ src/signatures/ateliersjacob/index.js
@@ -11,8 +11,9 @@ import instagramIcon from './assets/instagram.png';
 */
 
 const border = process.env.PUBLIC_URL + '/ateliersjacob/assets/border.png';
-const logoAJ = process.env.PUBLIC_URL +  '/ateliersjacob/assets/cuisirama.jpg';
+const logoAJ = process.env.PUBLIC_URL +  '/ateliersjacob/assets/ateliersjacob.png';
 const logoAM = process.env.PUBLIC_URL +  '/ateliersjacob/assets/logo-airmiles.jpg';
+const logoCuisirama = process.env.PUBLIC_URL +  '/ateliersjacob/assets/cuisirama.jpg';
 const logoT = process.env.PUBLIC_URL +  '/ateliersjacob/assets/tendances.png';
 const facebookIcon = process.env.PUBLIC_URL +  '/ateliersjacob/assets/facebook.png';
 const instagramIcon = process.env.PUBLIC_URL +  '/ateliersjacob/assets/instagram.png';
@@ -188,14 +189,15 @@ class AteliersJacob extends React.Component {
             <Table width={table.innerWidth}>
               <tr>
                 <td width="49%" style={styles.logo}>
-                  <a href="http://www.cuisirama.qc.ca/fr/" target="_blank"><img src={logoAJ} width={100} /></a></td>
+                  <a href="https://www.ateliersjacob.com/" target="_blank"><img src={logoAJ} /></a><br/><br/>
+                  <a href="https://www.facebook.com/AteliersJacob/" target="_blank"><img src={facebookIcon} /></a>&nbsp;&nbsp;
+                  <a href="https://www.instagram.com/ateliersjacob/" target="_blank"><img src={instagramIcon} /></a>
+                </td>
                 <td width="2%" align="center">
                   <img src={border} />
                 </td>
                 <td width="49%" style={styles.logo}>
-                  <a href="http://www.tendances-concept.com/" target="_blank"><img src={logoT} /></a><br/>
-                  <a href="https://www.facebook.com/tendancesconcept/" target="_blank"><img src={facebookIcon} /></a>&nbsp;&nbsp;
-                  <a href="https://www.instagram.com/tendances.concept/" target="_blank"><img src={instagramIcon} /></a>
+                  <a href="http://www.cuisirama.qc.ca/fr/" target="_blank"><img src={logoCuisirama} width={100} /></a>
                 </td>
               </tr>
             </Table>
@@ -220,10 +222,8 @@ class AteliersJacob extends React.Component {
                   :
                   <Table width={table.innerWidth}>
                     <tr>
-                      <td width={60}>
-                        <img src={logoAM} />
-                      </td>
                       <td>
+                        <br/>
                         <Table width="100%">
                           <tr>
                             <td style={styles.footer}>
