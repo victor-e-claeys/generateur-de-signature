b7ad4ee642fd629f27490fb7d9afbafee97295f4
Vincent Claeys <ecrire@victorconceptum.com>
2020-12-23 12:09:46 -0500

    Renard

--- src/signatures/index.js
+++ src/signatures/index.js
@@ -3,6 +3,7 @@ import BlancMarine from './blancmarine';
 import CapsuleCreative from './capsulecreative';
 import GroupeE5 from './groupee5';
 import Montis from './montis';
+import Renard from './renard';
 import VictorConceptum from './victorconceptum';
 
-export {AteliersJacob, BlancMarine, CapsuleCreative, GroupeE5, Montis, VictorConceptum}
+export {AteliersJacob, BlancMarine, CapsuleCreative, GroupeE5, Montis, Renard, VictorConceptum}
